74b9b91dde8477571b8da34e6b7ea53be0f9ea12
williamndongmo88 <williamndongmo88@gmail.com>
2026-04-10 19:01:11 +0200

    feat:Implémentation de l'espace Client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,6 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    //provideZoneChangeDetection({ eventCoalescing: true } ),
     provideRouter(routes),
 
     // C'EST CETTE LIGNE QUI ACTIVE L'INTERCEPTEUR
