485a17f2825e55ad0601c168218fe95018b8b607
williamndongmo88 <williamndongmo88@gmail.com>
2026-04-16 06:03:52 +0200

    feat: Changement de l'url de la prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,10 @@ export const environment = {
   production: false, // Change to true to activate production environment
   apiUrlDev: 'http://localhost:8008/api',
   brokerURLDev: 'ws://localhost:8008/ws',
-  brokerURLProd: 'ws://medipay-solution-production.up.railway.app/ws',
-  apiUrlProd:'https://medipay-solution-production.up.railway.app/api'
+  brokerURLProd: 'ws://medipay-production.up.railway.app/ws',
+  apiUrlProd:'https://medipay-production.up.railway.app/api'//medipay-production.up.railway.app
 };
+
 if (environment.production) {
   console.log("✅ Environment de Prduction chargé !");
 }else{
